fdc26887f53961ce362a47370ed173ae07e8e301
Enio N. de Souza <enionsouza@gmail.com>
2021-09-14 22:45:36 -0400

    feat: Add 'Calculator' component with some styles

---
+++ src/components/Calculator.js
@@ -0,0 +1,19 @@
+const Calculator = () => {
+  const utilLabels = ['AC', '+/-', '%'];
+  const digitLabels = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+  const operatorLabels = ['/', 'x', '-', '+', '='];
+
+  const utils = utilLabels.map((utilLabel, index) => <div id={`util-${index}`} key={`${utilLabel}`} className="util">{utilLabel}</div>);
+  const digits = digitLabels.map((digitLabel, index) => <div key={`${digitLabel}`} id={`digit-${index}`} className="digit">{digitLabel}</div>);
+  const operators = operatorLabels.map((operatorLabel, index) => <div id={`operator-${index}`} key={`${operatorLabel}`} className="operator">{operatorLabel}</div>);
+  return (
+    <div className="grid">
+      <div className="display">0</div>
+      {utils}
+      {digits}
+      {operators}
+    </div>
+  );
+};
+
+export default Calculator;
