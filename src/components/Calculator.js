00e226140b4b3cb35e046d46e7a30dedf1909a80
Enio N. de Souza <enionsouza@gmail.com>
2021-09-15 14:59:36 -0400

    refactor: Refactor stylesheet

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,14 +5,14 @@ class Calculator extends React.Component {
   render() {
     const utilLabels = ['AC', '+/-', '%'];
     const digitLabels = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
-    const operatorLabels = ['/', 'x', '-', '+', '='];
+    const operatorLabels = ['÷', 'x', '-', '+', '='];
 
-    const utils = utilLabels.map((utilLabel, index) => <div id={`util-${index}`} key={`${utilLabel}`} className="util">{utilLabel}</div>);
-    const digits = digitLabels.map((digitLabel, index) => <div key={`${digitLabel}`} id={`digit-${index}`} className="digit">{digitLabel}</div>);
-    const operators = operatorLabels.map((operatorLabel, index) => <div id={`operator-${index}`} key={`${operatorLabel}`} className="operator">{operatorLabel}</div>);
+    const utils = utilLabels.map((utilLabel, index) => <div id={`util-${index}`} key={`${utilLabel}`} style={{ gridArea: `util-${index}` }} className="util">{utilLabel}</div>);
+    const digits = digitLabels.map((digitLabel, index) => <div key={`${digitLabel}`} id={`digit-${index}`} style={{ gridArea: `digit-${index}` }} className="digit">{digitLabel}</div>);
+    const operators = operatorLabels.map((operatorLabel, index) => <div id={`operator-${index}`} key={`${operatorLabel}`} style={{ gridArea: `operator-${index}` }} className="operator">{operatorLabel}</div>);
     return (
       <div className="grid">
-        <div className="display">0</div>
+        <div style={{ gridArea: 'display' }} className="display">0</div>
         {utils}
         {digits}
         {operators}
