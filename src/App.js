065a9b01a045bcac2b500936f8343d937255da61
Enio N. de Souza <enionsouza@gmail.com>
2021-09-14 23:17:22 -0400

    chore: Use class based components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app-container">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
